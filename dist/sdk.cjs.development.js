709c7502a9788703adb4d8e75e0cd80663966c22
magikfinance <97064767+magikfinance@users.noreply.github.com>
2022-08-20 16:24:02 +0100

    update

--- dist/sdk.cjs.development.js
+++ dist/sdk.cjs.development.js
@@ -21,7 +21,7 @@ var _SOLIDITY_TYPE_MAXIMA;
 
 (function (ChainId) {
   ChainId[ChainId["MAINNET"] = 2000] = "MAINNET";
-  ChainId[ChainId["TESTNET"] = 568] = "TESTNET";
+  ChainId[ChainId["TESTNET"] = 538] = "TESTNET";
 })(exports.ChainId || (exports.ChainId = {}));
 
 (function (TradeType) {
@@ -35,12 +35,9 @@ var _SOLIDITY_TYPE_MAXIMA;
   Rounding[Rounding["ROUND_UP"] = 2] = "ROUND_UP";
 })(exports.Rounding || (exports.Rounding = {}));
 
-var FACTORY_ADDRESS = '0x1e00cf5647108EdCB4f6e1EbC5805753db3dF63e';
+var FACTORY_ADDRESS = '0x1e00cf5647108EdCB4f6e1EbC5805753db3dF63e'; //TODO Need to change this. This address should be same for both testnet and mainnet
+
 var INIT_CODE_HASH = '0x775ea03d1cb61415532ec1702446ff6fb3b9da5c21217900e1e8882100bf489f';
-var SPOOKY_FACTORY_ADDRESS = '0x152eE697f2E276fA89E96742e9bB9aB1F2E61bE3';
-var SPOOKY_INIT_CODE_HASH = '0xcdf2deca40a0bd56de8e3ce5c7df6727e5b1bf2ac96f283fa9c4b3e6b42ea9d2';
-var SUSHI_FACTORY_ADDRESS = "0xc35DADB65012eC5796536bD9864eD8773aBc74C4";
-var SUSHI_INIT_CODE_HASH = "0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303";
 var MINIMUM_LIQUIDITY = /*#__PURE__*/JSBI.BigInt(1000); // exports for internal consumption
 
 var ZERO = /*#__PURE__*/JSBI.BigInt(0);
@@ -373,7 +370,7 @@ function Currency(decimals, symbol, name) {
  * The only instance of the base class `Currency`.
  */
 
-Currency.ETHER = /*#__PURE__*/new Currency(18, 'DOGE', 'Doge Coin');
+Currency.ETHER = /*#__PURE__*/new Currency(18, 'FTM', 'Fantom');
 var ETHER = Currency.ETHER;
 
 var _WETH;
@@ -439,7 +436,7 @@ function currencyEquals(currencyA, currencyB) {
     return currencyA === currencyB;
   }
 }
-var WETH = (_WETH = {}, _WETH[exports.ChainId.MAINNET] = /*#__PURE__*/new Token(exports.ChainId.MAINNET, '0xb7ddc6414bf4f5515b52d8bdd69973ae205ff101', 18, 'wwDoge', 'Wrapped Doge'), _WETH[exports.ChainId.TESTNET] = /*#__PURE__*/new Token(exports.ChainId.TESTNET, '0xf1277d1ed8ad466beddf92ef448a132661956621', 18, 'wFTM', 'Wrapped Fantom'), _WETH);
+var WETH = (_WETH = {}, _WETH[exports.ChainId.MAINNET] = /*#__PURE__*/new Token(exports.ChainId.MAINNET, '0xb7ddc6414bf4f5515b52d8bdd69973ae205ff101', 18, 'wwDoge', 'Wrapped Fantom'), _WETH[exports.ChainId.TESTNET] = /*#__PURE__*/new Token(exports.ChainId.TESTNET, '0xb7ddc6414bf4f5515b52d8bdd69973ae205ff101', 18, 'wwDoge', 'Wrapped Fantom'), _WETH);
 
 var _toSignificantRoundin, _toFixedRounding;
 var Decimal = /*#__PURE__*/toFormat(_Decimal);
@@ -758,8 +755,6 @@ var Price = /*#__PURE__*/function (_Fraction) {
 }(Fraction);
 
 var PAIR_ADDRESS_CACHE = {};
-var SPOOKY_PAIR_ADDRESS_CACHE = {};
-var SUSHI_PAIR_ADDRESS_CACHE = {};
 var Pair = /*#__PURE__*/function () {
   function Pair(tokenAmountA, tokenAmountB) {
     var tokenAmounts = tokenAmountA.token.sortsBefore(tokenAmountB.token) // does safety checks
@@ -780,34 +775,6 @@ var Pair = /*#__PURE__*/function () {
     }
 
     return PAIR_ADDRESS_CACHE[tokens[0].address][tokens[1].address];
-  };
-
-  Pair.getSpookyAddress = function getSpookyAddress(tokenA, tokenB) {
-    var _SPOOKY_PAIR_ADDRESS_, _SPOOKY_PAIR_ADDRESS_2;
-
-    var tokens = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA]; // does safety checks
-
-    if (((_SPOOKY_PAIR_ADDRESS_ = SPOOKY_PAIR_ADDRESS_CACHE) === null || _SPOOKY_PAIR_ADDRESS_ === void 0 ? void 0 : (_SPOOKY_PAIR_ADDRESS_2 = _SPOOKY_PAIR_ADDRESS_[tokens[0].address]) === null || _SPOOKY_PAIR_ADDRESS_2 === void 0 ? void 0 : _SPOOKY_PAIR_ADDRESS_2[tokens[1].address]) === undefined) {
-      var _SPOOKY_PAIR_ADDRESS_3, _extends4, _extends5;
-
-      SPOOKY_PAIR_ADDRESS_CACHE = _extends({}, SPOOKY_PAIR_ADDRESS_CACHE, (_extends5 = {}, _extends5[tokens[0].address] = _extends({}, (_SPOOKY_PAIR_ADDRESS_3 = SPOOKY_PAIR_ADDRESS_CACHE) === null || _SPOOKY_PAIR_ADDRESS_3 === void 0 ? void 0 : _SPOOKY_PAIR_ADDRESS_3[tokens[0].address], (_extends4 = {}, _extends4[tokens[1].address] = address.getCreate2Address(SPOOKY_FACTORY_ADDRESS, solidity.keccak256(['bytes'], [solidity.pack(['address', 'address'], [tokens[0].address, tokens[1].address])]), SPOOKY_INIT_CODE_HASH), _extends4)), _extends5));
-    }
-
-    return SPOOKY_PAIR_ADDRESS_CACHE[tokens[0].address][tokens[1].address];
-  };
-
-  Pair.getSushiAddress = function getSushiAddress(tokenA, tokenB) {
-    var _SUSHI_PAIR_ADDRESS_C, _SUSHI_PAIR_ADDRESS_C2;
-
-    var tokens = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA]; // does safety checks
-
-    if (((_SUSHI_PAIR_ADDRESS_C = SUSHI_PAIR_ADDRESS_CACHE) === null || _SUSHI_PAIR_ADDRESS_C === void 0 ? void 0 : (_SUSHI_PAIR_ADDRESS_C2 = _SUSHI_PAIR_ADDRESS_C[tokens[0].address]) === null || _SUSHI_PAIR_ADDRESS_C2 === void 0 ? void 0 : _SUSHI_PAIR_ADDRESS_C2[tokens[1].address]) === undefined) {
-      var _SUSHI_PAIR_ADDRESS_C3, _extends6, _extends7;
-
-      SUSHI_PAIR_ADDRESS_CACHE = _extends({}, SUSHI_PAIR_ADDRESS_CACHE, (_extends7 = {}, _extends7[tokens[0].address] = _extends({}, (_SUSHI_PAIR_ADDRESS_C3 = SUSHI_PAIR_ADDRESS_CACHE) === null || _SUSHI_PAIR_ADDRESS_C3 === void 0 ? void 0 : _SUSHI_PAIR_ADDRESS_C3[tokens[0].address], (_extends6 = {}, _extends6[tokens[1].address] = address.getCreate2Address(SUSHI_FACTORY_ADDRESS, solidity.keccak256(['bytes'], [solidity.pack(['address', 'address'], [tokens[0].address, tokens[1].address])]), SUSHI_INIT_CODE_HASH), _extends6)), _extends7));
-    }
-
-    return SUSHI_PAIR_ADDRESS_CACHE[tokens[0].address][tokens[1].address];
   }
   /**
    * Returns true if the token is either token0 or token1
--- src/constants.ts
+++ src/constants.ts
@@ -4,8 +4,9 @@ import JSBI from 'jsbi'
 export type BigintIsh = JSBI | bigint | string
 
 export enum ChainId {
+  
   MAINNET = 2000,
-  TESTNET = 568
+  TESTNET = 538
 }
 
 export enum TradeType {
@@ -19,15 +20,9 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0xAaA04462e35f3e40D798331657cA015169e005d7'
+export const FACTORY_ADDRESS = '0x1e00cf5647108EdCB4f6e1EbC5805753db3dF63e' //TODO Need to change this. This address should be same for both testnet and mainnet
 
-export const INIT_CODE_HASH = '0x746cdd6c24c5f1496602686ccbb848d5d9c8e5ccc408250c4cc8be8331de6857'
-
-export const SPOOKY_FACTORY_ADDRESS = '0x152eE697f2E276fA89E96742e9bB9aB1F2E61bE3'
-export const SPOOKY_INIT_CODE_HASH = '0xcdf2deca40a0bd56de8e3ce5c7df6727e5b1bf2ac96f283fa9c4b3e6b42ea9d2'
-
-export const SUSHI_FACTORY_ADDRESS = "0xc35DADB65012eC5796536bD9864eD8773aBc74C4"
-export const SUSHI_INIT_CODE_HASH = "0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303"
+export const INIT_CODE_HASH = '0x775ea03d1cb61415532ec1702446ff6fb3b9da5c21217900e1e8882100bf489f'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
