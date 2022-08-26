25f2f380bed38e4b710ca6b009f138e9ab3f996a
magikfinance <97064767+magikfinance@users.noreply.github.com>
2022-08-26 22:19:56 +0100

    Revert "update"
    This reverts commit 709c7502a9788703adb4d8e75e0cd80663966c22.

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -2,7 +2,7 @@ import JSBI from 'jsbi';
 export declare type BigintIsh = JSBI | bigint | string;
 export declare enum ChainId {
     MAINNET = 2000,
-    TESTNET = 538
+    TESTNET = 568
 }
 export declare enum TradeType {
     EXACT_INPUT = 0,
@@ -15,6 +15,10 @@ export declare enum Rounding {
 }
 export declare const FACTORY_ADDRESS = "0x1e00cf5647108EdCB4f6e1EbC5805753db3dF63e";
 export declare const INIT_CODE_HASH = "0x775ea03d1cb61415532ec1702446ff6fb3b9da5c21217900e1e8882100bf489f";
+export declare const SPOOKY_FACTORY_ADDRESS = "0x152eE697f2E276fA89E96742e9bB9aB1F2E61bE3";
+export declare const SPOOKY_INIT_CODE_HASH = "0xcdf2deca40a0bd56de8e3ce5c7df6727e5b1bf2ac96f283fa9c4b3e6b42ea9d2";
+export declare const SUSHI_FACTORY_ADDRESS = "0xc35DADB65012eC5796536bD9864eD8773aBc74C4";
+export declare const SUSHI_INIT_CODE_HASH = "0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303";
 export declare const MINIMUM_LIQUIDITY: JSBI;
 export declare const ZERO: JSBI;
 export declare const ONE: JSBI;
--- dist/entities/token.d.ts
+++ dist/entities/token.d.ts
@@ -1,30 +1,74 @@
-import { ChainId } from '../constants';
-import { Currency } from './currency';
+import invariant from 'tiny-invariant'
+import { ChainId } from '../constants'
+import { validateAndParseAddress } from '../utils'
+import { Currency } from './currency'
+
 /**
  * Represents an ERC20 token with a unique address and some metadata.
  */
-export declare class Token extends Currency {
-    readonly chainId: ChainId;
-    readonly address: string;
-    constructor(chainId: ChainId, address: string, decimals: number, symbol?: string, name?: string);
-    /**
-     * Returns true if the two tokens are equivalent, i.e. have the same chainId and address.
-     * @param other other token to compare
-     */
-    equals(other: Token): boolean;
-    /**
-     * Returns true if the address of this token sorts before the address of the other token
-     * @param other other token to compare
-     * @throws if the tokens have the same address
-     * @throws if the tokens are on different chains
-     */
-    sortsBefore(other: Token): boolean;
+export class Token extends Currency {
+  public readonly chainId: ChainId
+  public readonly address: string
+
+  public constructor(chainId: ChainId, address: string, decimals: number, symbol?: string, name?: string) {
+    super(decimals, symbol, name)
+    this.chainId = chainId
+    this.address = validateAndParseAddress(address)
+  }
+
+  /**
+   * Returns true if the two tokens are equivalent, i.e. have the same chainId and address.
+   * @param other other token to compare
+   */
+  public equals(other: Token): boolean {
+    // short circuit on reference equality
+    if (this === other) {
+      return true
+    }
+    return this.chainId === other.chainId && this.address === other.address
+  }
+
+  /**
+   * Returns true if the address of this token sorts before the address of the other token
+   * @param other other token to compare
+   * @throws if the tokens have the same address
+   * @throws if the tokens are on different chains
+   */
+  public sortsBefore(other: Token): boolean {
+    invariant(this.chainId === other.chainId, 'CHAIN_IDS')
+    invariant(this.address !== other.address, 'ADDRESSES')
+    return this.address.toLowerCase() < other.address.toLowerCase()
+  }
 }
+
 /**
  * Compares two currencies for equality
  */
-export declare function currencyEquals(currencyA: Currency, currencyB: Currency): boolean;
-export declare const WETH: {
-    2000: Token;
-    538: Token;
-};
+export function currencyEquals(currencyA: Currency, currencyB: Currency): boolean {
+  if (currencyA instanceof Token && currencyB instanceof Token) {
+    return currencyA.equals(currencyB)
+  } else if (currencyA instanceof Token) {
+    return false
+  } else if (currencyB instanceof Token) {
+    return false
+  } else {
+    return currencyA === currencyB
+  }
+}
+
+export const WETH = {
+  [ChainId.MAINNET]: new Token(
+    ChainId.MAINNET,
+    '0xb7ddc6414bf4f5515b52d8bdd69973ae205ff101',
+    18,
+    'WWDOGE',
+    'Wrapped Doge'
+  ),
+  [ChainId.TESTNET]: new Token(
+    ChainId.TESTNET,
+    '0x2465086E721F68761e3275A54802C985FFd0D727',
+    18,
+    'WWDOGE',
+    'Wrapped Doge'
+  )
+}
